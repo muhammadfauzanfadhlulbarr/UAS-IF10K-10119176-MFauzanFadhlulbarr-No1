50421113419070ec718a5a3b776ff7f4a67ab890
Muhammad Fauzan Fadhlulbarr <52595452+muhammadfauzanfadhlulbarr@users.noreply.github.com>
2021-02-19 21:04:18 +0700

    Create UAS-IF10K-10119176-MFauzanFadhlulbarr-No1.java

---
+++ UAS-IF10K-10119176-MFauzanFadhlulbarr-No1.java
@@ -0,0 +1,22 @@
+/**
+ * Latihan36
+ * Nama     : Muhammad Fauzan Fadhlulbarr
+ * Kelas    : PBO10K
+ * NIM      : 10119176
+ * No Soal  : 1
+ */
+
+public class UAS{
+
+    public static void main(String[] args) {
+        nilai nilai = new nilai();
+
+        nilai.setNilai(45,5)
+
+        System.out.println("Masukan Nilai Akhir Anda  = " + nilai.getNA());
+        System.out.println("Index Anda Adalah  = " + nilai.Index());
+        
+
+    }
+
+}
